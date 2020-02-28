9e992e75ac16d8e0212189282598e3034bb03d43
davidruvolo51 <davidruvolo51@gmail.com>
2020-02-28 16:30:33 +0100

    adding rss feed

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,6 @@ const path = require("path")
 const { createFilePath, createFileNode } = require(`gatsby-source-filesystem`)
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions
-    // const blogPostTemplate = path.resolve(`./src/components/tutorials.js`)
     return new Promise((resolve, reject) => {
         resolve(graphql(`
     {
